982ce0a2183339155cee5f670c4b127e3ccecfae
lingyun <lingyun@cambricon.com>
2018-06-03 10:17:50 +0800

    move package outside of the build directory

--- external.mk
+++ external.mk
@@ -5,7 +5,7 @@ include $(BR2_EXTERNAL_cambricon_buildroot_PATH)/boot/common.mk
 
 include $(BR2_EXTERNAL_cambricon_buildroot_PATH)/linux/linux.mk
 
-include $(sort $(wildcard $(BR2_EXTERNAL_cambricon_buildroot_PATH)/package/*/cambricon_build.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_cambricon_buildroot_PATH)/../package/*/cambricon_build.mk))
 
 
 lingyun:
