80d51c525622ecd0ed548df1dddac50518de695a
lingyun <lingyun@cambricon.com>
2018-06-03 20:33:57 +0800

    1231

--- external.mk
+++ external.mk
@@ -1,4 +1,4 @@
-SOURCE_CODE_PATH=$(BR2_EXTERNAL_cambricon_buildroot_PATH)/../src
+SOURCE_CODE_PATH=$(BR2_EXTERNAL_cambricon_buildroot_PATH)/../example
 
 #Rules for building packages
 
@@ -12,6 +12,7 @@ include $(sort $(wildcard $(SOURCE_CODE_PATH)/*/cambricon_build.mk))
 
 
 lingyun:
-	@$(call MESSAGE,"####LINGYUN####"$(LINUX_DIR))
+	@$(call MESSAGE,"####LINGYUN####"$(HOST_EXAMPLE2_OVERRIDE_SRCDIR))
+	@$(call MESSAGE,"####LINGYUN####"$(HOST_EXAMPLE2_SITE))
 	
 	
