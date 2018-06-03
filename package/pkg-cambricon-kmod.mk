a3bbcc82d28c817e09a778232ad2efaded211f3b
lingyun <lingyun@cambricon.com>
2018-06-03 13:00:42 +0800

    support build kernel modules for HOST

--- package/pkg-cambricon-kmod.mk
+++ package/pkg-cambricon-kmod.mk
@@ -19,10 +19,106 @@
 #   $(eval $(cambricon-arm-generic-package))
 ################################################################################
 
+HOST_LINUX_DIR = /lib/modules/$(shell uname -r)/build
 
+HOST_LINUX_MAKE_FLAGS = \
+	HOSTCC="$(HOSTCC) $(HOST_CFLAGS) $(HOST_LDFLAGS)" \
+	INSTALL_MOD_PATH=$(HOST_OUTPUT) \
+	DEPMOD=$(HOST_DIR)/sbin/depmod
+
+
+################################################################################
+# argument 1 is the lowercase package name
+# argument 2 is the uppercase package name
+################################################################################
+define cambricon-host-inner-kernel-module
+
+# This is only defined in some infrastructures (e.g. autotools, cmake),
+# but not in others (e.g. generic). So define it here as well.
+$(2)_MAKE ?= $$(MAKE)
+
+# If not specified, consider the source of the kernel module to be at
+# the root of the package.
+$(2)_MODULE_SUBDIRS ?= .
+
+#
+# Process _BUILD_MAKE_OPT
+#
+ifdef HOST_MODULE_MAKE_OPTS
+  $(2)_MODULE_MAKE_OPTS += $(HOST_MODULE_MAKE_OPTS)
+endif
+
+ifdef HOST_MODULE_SUBDIRS
+  $(2)_MODULE_SUBDIRS += $(HOST_MODULE_SUBDIRS)
+endif
+
+# Build the kernel module(s)
+# Force PWD for those packages that want to use it to find their
+# includes and other support files (Booo!)
+define $(2)_KERNEL_MODULES_BUILD
+	@$$(call MESSAGE,"Building kernel module(s)")
+	$$(foreach d,$$($(2)_MODULE_SUBDIRS), \
+		$$(LINUX_MAKE_ENV) $$($$(PKG)_MAKE) \
+			-C $$(HOST_LINUX_DIR) \
+			$$(HOST_LINUX_MAKE_FLAGS) \
+			$$($(2)_MODULE_MAKE_OPTS) \
+			PWD=$$(@D)/$$(d) \
+			M=$$(@D)/$$(d) \
+			modules$$(sep))
+endef
+$(2)_POST_BUILD_HOOKS += $(2)_KERNEL_MODULES_BUILD
+
+# Install the kernel module(s)
+# Force PWD for those packages that want to use it to find their
+# includes and other support files (Booo!)
+define $(2)_KERNEL_MODULES_INSTALL
+	@$$(call MESSAGE,"Installing kernel module(s)")
+	$$(foreach d,$$($(2)_MODULE_SUBDIRS), \
+		$$(LINUX_MAKE_ENV) $$($$(PKG)_MAKE) \
+			-C $$(HOST_LINUX_DIR) \
+			$$(HOST_LINUX_MAKE_FLAGS) \
+			$$($(2)_MODULE_MAKE_OPTS) \
+			PWD=$$(@D)/$$(d) \
+			M=$$(@D)/$$(d) \
+			modules_install$$(sep))
+endef
+$(2)_POST_INSTALL_HOOKS += $(2)_KERNEL_MODULES_INSTALL
+
+# undefine the reference
+HOST_MODULE_MAKE_OPTS =
+HOST_MODULE_SUBDIRS =
+
+endef
+
+################################################################################
+# argument 1 is the lowercase package name
+# argument 2 is the uppercase package name
+################################################################################
+define cambricon-arm-inner-kernel-module
+
+#
+# Process _BUILD_MAKE_OPT
+#
+ifdef ARM_MODULE_MAKE_OPTS
+  $(2)_MODULE_MAKE_OPTS += $(ARM_MODULE_MAKE_OPTS)
+endif
+
+ifdef ARM_MODULE_SUBDIRS
+  $(2)_MODULE_SUBDIRS += $(ARM_MODULE_SUBDIRS)
+endif
+
+$(call inner-kernel-module,$(1),$(2))
+
+# undefine the reference
+ARM_MODULE_MAKE_OPTS =
+ARM_MODULE_SUBDIRS =
+
+endef
 
 ################################################################################
 # kernel-module -- the target generator macro for kernel module packages
 ################################################################################
 
-cambricon-arm-kernel-module = $(call inner-kernel-module,$(pkgname),$(call UPPERCASE,$(pkgname)))
+cambricon-arm-kernel-module = $(call cambricon-arm-inner-kernel-module,$(pkgname),$(call UPPERCASE,$(pkgname)))
+
+cambricon-host-kernel-module = $(call cambricon-host-inner-kernel-module,host-$(pkgname),$(call UPPERCASE,host-$(pkgname)))
